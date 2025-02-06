9c22405a3cbaa03e7abaeaa2766c4d44013d5b60
Paul Filitchkin <filitchp@gmail.com>
2025-02-05 22:39:09 -0800

    Added CLI script for matching image pairs and computing homography

--- match_and_compute_homography.py
+++ match_and_compute_homography.py
@@ -1,41 +1,37 @@
+import argparse
 import json
-import os
-os.chdir("..")
+import os.path as op
 from copy import deepcopy
 
 import torch
 import cv2
 import numpy as np
-import matplotlib.cm as cm
-from src.utils.plotting import make_matching_figure
+
+# os.chdir("..")
 from src.loftr import LoFTR, full_default_cfg, opt_default_cfg, reparameter
 
-global _matcher, _precision
+global _matcher, _PRECISION
 
 # You can choose numerical precision in ['fp32', 'mp', 'fp16']. 'fp16' for best efficiency
-_precision = 'fp32'  # Enjoy near-lossless precision with Mixed Precision (MP) / FP16 computation if you have a modern GPU (recommended NVIDIA architecture >= SM_70).
+_PRECISION = 'fp32'  # Enjoy near-lossless precision with Mixed Precision (MP) / FP16 computation if you have a modern GPU (recommended NVIDIA architecture >= SM_70).
 
 # You can choose model type in ['full', 'opt']
-_model_type = 'full'  # 'full' for best quality, 'opt' for best efficiency
-
-_root = '../M3M_WinterBarley_Dataset/90m_RGBMSP_RTK_Overlap7080/Dataset/'
-_img0_pth = os.path.join(_root, 'DJI_20240405154706_0001_D.JPG')
-_img1_pth = os.path.join(_root, 'DJI_20240405154706_0001_MS_NIR.TIF')
+_MODEL_TYPE = 'full'  # 'full' for best quality, 'opt' for best efficiency
 
 def configure_model():
-    global _matcher, _precision, _model_type
+    global _matcher, _PRECISION, _MODEL_TYPE
 
     # You can also change the default values like thr. and npe (based on input image size)
-    if _model_type == 'full':
+    if _MODEL_TYPE == 'full':
         config = deepcopy(full_default_cfg)
-    elif _model_type == 'opt':
+    elif _MODEL_TYPE == 'opt':
         config = deepcopy(opt_default_cfg)
     else:
-        raise Exception(f'Invalid model type: {_model_type}')
+        raise Exception(f'Invalid model type: {_MODEL_TYPE}')
 
-    if _precision == 'mp':
+    if _PRECISION == 'mp':
         config['mp'] = True
-    elif _precision == 'fp16':
+    elif _PRECISION == 'fp16':
         config['half'] = True
 
     print(config)
@@ -44,33 +40,60 @@ def configure_model():
     _matcher.load_state_dict(torch.load("weights/eloftr_outdoor.ckpt")['state_dict'])
     _matcher = reparameter(_matcher)  # no reparameterization will lead to low performance
 
-    if _precision == 'fp16':
+    if _PRECISION == 'fp16':
         _matcher = _matcher.half()
 
     _matcher = _matcher.eval().cuda()
 
 
-def load_images():
-    global _matcher, _precision, _img0_pth, _img1_pth
+def resize_image_to_multiple_of_32(image: np.ndarray, max_width: int):
+    """
+    This will limit size and crop (unlike the original example)
+    TODO: this assume the image is wide, should handle tall images
+    """
 
-    img0_raw: np.ndarray = cv2.imread(_img0_pth, cv2.IMREAD_GRAYSCALE)
-    img1_raw: np.ndarray = cv2.imread(_img1_pth, cv2.IMREAD_GRAYSCALE)
+    # Get original dimensions
+    original_height, original_width = image.shape[:2]
 
-    # input size should be divisible by 32
-    # Scale them down or else we run out of GPU memory
-    h0 = int((img0_raw.shape[1] / 4) // 32 * 32)
-    w0 = int((img0_raw.shape[0] / 4) // 32 * 32)
-    h1 = int((img1_raw.shape[1] / 2) // 32 * 32)
-    w1 = int((img1_raw.shape[0] / 2) // 32 * 32)
+    # Compute original aspect ratio
+    aspect_ratio = original_width / original_height
 
-    img0_raw = cv2.resize(img0_raw, (h0, w0))
-    img1_raw = cv2.resize(img1_raw, (h1, w1))
+    max_width_mult = max_width // 32
 
-    return img0_raw, img1_raw
+    # Compute new dimensions as multiples of 32
+    width_mult = original_width // 32
+
+    if width_mult > max_width_mult:
+        # Max that's a multiple of 32
+        width_mult = max_width_mult
+    new_width = width_mult * 32
+
+    new_height = int(np.round(new_width / aspect_ratio))
+
+    resized_image = cv2.resize(image, (new_width, new_height))
+
+    # Crop out height
+    if resized_image.shape[0] % 32 != 0:
+        crop_height = (resized_image.shape[0] // 32) * 32
+        print(resized_image.shape[0])
+        return resized_image[:crop_height, :]
+
+    return resized_image
+
+def load_images(img0_path: str, img1_path: str) -> tuple[np.ndarray, np.ndarray]:
+    global _matcher, _PRECISION
+
+    img0_raw: np.ndarray = cv2.imread(img0_path)
+    img1_raw: np.ndarray = cv2.imread(img1_path)
+
+    img0_resized = resize_image_to_multiple_of_32(img0_raw, 1024)
+    img1_resized = resize_image_to_multiple_of_32(img1_raw, 1024)
+
+    return img0_resized, img1_resized
 
 
 def run(img0_raw: np.ndarray, img1_raw: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    if _precision == 'fp16':
+    if _PRECISION == 'fp16':
         img0 = torch.from_numpy(img0_raw)[None][None].half().cuda() / 255.
         img1 = torch.from_numpy(img1_raw)[None][None].half().cuda() / 255.
     else:
@@ -81,7 +104,7 @@ def run(img0_raw: np.ndarray, img1_raw: np.ndarray) -> tuple[np.ndarray, np.ndar
 
     # Inference with EfficientLoFTR and get prediction
     with torch.no_grad():
-        if _precision == 'mp':
+        if _PRECISION == 'mp':
             with torch.autocast(enabled=True, device_type='cuda'):
                 _matcher(batch)
         else:
@@ -93,7 +116,7 @@ def run(img0_raw: np.ndarray, img1_raw: np.ndarray) -> tuple[np.ndarray, np.ndar
         return mkpts0, mkpts1, mconf
 
 
-def draw(img1, img2, points1, points2, conf, thresh, output_path):
+def draw_matches(img1, img2, points1, points2, conf, thresh, output_path):
     """
     (rewrote original to work without matplotlib)
     Draws two images side by side with lines connecting corresponding points and saves the result.
@@ -111,6 +134,7 @@ def draw(img1, img2, points1, points2, conf, thresh, output_path):
     # Convert single-channel images to BGR
     if len(img1.shape) == 2:
         img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
+
     if len(img2.shape) == 2:
         img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
 
@@ -152,7 +176,7 @@ def compute_homography(source_points: np.ndarray, target_points: np.ndarray, sou
     # Extract source and target points from self.all_pts
 
     # Compute the homography matrix
-    h, _ = cv2.findHomography(source_points, target_points)
+    h, mask = cv2.findHomography(source_points, target_points)
 
     # Get source image dimensions
     height, width = source_shape[:2]
@@ -174,13 +198,48 @@ def compute_homography(source_points: np.ndarray, target_points: np.ndarray, sou
     # Return the homography matrix and the transformed corners
     return h, tuple(np.round(normalized_corners).astype(int))
 
+
+def validate_inputs(img_path: str, img0: str, img1: str):
+    # Check if img_path is a valid directory
+    if not op.isdir(img_path):
+        raise ValueError(f"Error: The path '{img_path}' is not a valid directory.")
+
+    # Check if img0 and img1 are valid files inside img_path
+    img0_path = op.join(img_path, img0)
+    img1_path = op.join(img_path, img1)
+
+    if not op.isfile(img0_path):
+        raise ValueError(f"Error: The file '{img0}' does not exist in '{img_path}'.")
+    if not op.isfile(img1_path):
+        raise ValueError(f"Error: The file '{img1}' does not exist in '{img_path}'.")
+
+    return img0_path, img1_path
+
+
 def main():
+    """
+    _root = '../M3M_WinterBarley_Dataset/90m_RGBMSP_RTK_Overlap7080/Dataset/'
+    _img0_path = op.join(_root, 'DJI_20240405154706_0001_D.JPG')
+    _img1_path = op.join(_root, 'DJI_20240405154706_0001_MS_G.TIF')
+    """
+    parser = argparse.ArgumentParser(description="Run EfficientLoFTR on a pair of images and compute homography")
+    parser.add_argument("img_path", type=str, help="Path to the directory containing images.")
+    parser.add_argument("img0", type=str, help="Name of the first image file.")
+    parser.add_argument("img1", type=str, help="Name of the second image file.")
+    parser.add_argument("output_path", type=str, help="Path to the output directory")
+
+    args = parser.parse_args()
+
+    img0_path, img1_path = validate_inputs(args.img_path, args.img0, args.img1)
+    
     configure_model()
-    (img0_resized, img1_resized) = load_images()
-    (mkpts0, mkpts1, mconf) = run(img0_resized, img1_resized)
+    (img0_resized, img1_resized) = load_images(img0_path, img1_path)
+    img0_resized_grey = cv2.cvtColor(img0_resized, cv2.COLOR_BGR2GRAY)
+    img1_resized_grey = cv2.cvtColor(img1_resized, cv2.COLOR_BGR2GRAY)
+    (mkpts0, mkpts1, mconf) = run(img0_resized_grey, img1_resized_grey)
 
-    i = np.where(mconf > 0.999)
-
+    # Filter by score
+    i = np.where(mconf > 0.1)
     mkpts0_filtered = mkpts0[i]  # RGB
     mkpts1_filtered = mkpts1[i]  # Other
 
@@ -188,17 +247,26 @@ def main():
     print(h)
     print(corners_dest)
 
-    draw(img0_resized, img1_resized, mkpts0, mkpts1, mconf, thresh=0.999, output_path='matches.png')
+    resize0_name = op.basename(img0_path)
+    resize0_split = op.splitext(resize0_name)
+    resize0_name = f'{resize0_split[0]}_resized{resize0_split[1]}'
+    resize1_name = op.basename(img1_path)
+    resize1_split = op.splitext(resize1_name)
+    resize1_name = f'{resize1_split[0]}_resized{resize1_split[1]}'
 
-    cv2.imwrite("rgb.jpg", img0_resized)
-    cv2.imwrite("nir.jpg", img1_resized)
+    cv2.imwrite(resize0_name, img0_resized)
+    cv2.imwrite(resize1_name, img1_resized)
+
+    match_name = f'matches_{resize0_split[0]}_{resize1_split[0]}.png'
+
+    draw_matches(img0_resized, img1_resized, mkpts0, mkpts1, mconf, thresh=0.1, output_path=match_name)
 
     perspective_config = {
         "corners_src": {
             "tl": [0, 0],
-            "tr": [img0_resized.shape[1], 0],
-            "bl": [0, img0_resized.shape[0]],
-            "br": [img0_resized.shape[1], img0_resized.shape[0]],
+            "tr": [img1_resized.shape[1], 0],
+            "bl": [0, img1_resized.shape[0]],
+            "br": [img1_resized.shape[1], img1_resized.shape[0]],
         },
         "corners_dest": {
             "tl": np.round(corners_dest[0][:2]).astype(int).tolist(),
@@ -209,7 +277,7 @@ def main():
     }
 
     with open('perspective_config.json', 'w') as f:
-        json.dump(perspective_config, f)
+        json.dump(perspective_config, f, indent=2)
 
 
 if __name__ == '__main__':
